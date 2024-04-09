38c0c2246ae41e61f4bb86bb6618d13a2117a86c
Radu Nastas <nastas799@gmail.com>
2024-04-09 14:45:05 +0200

    commit new lesson

---
+++ lesson02_demo2/src/main/java/de/ait/controller/UserController.java
@@ -0,0 +1,12 @@
+package de.ait.controller;
+
+
+import de.ait.model.User;
+
+import java.util.List;
+
+public interface UserController {
+    List<User> getAllUsers();
+    User getUserByEmail();
+    void inputUser();
+}
